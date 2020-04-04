570926dfd8d233cbefc0a11331fef10c22f8f392
Ozan Sener <ozan@ozansener.com>
2020-04-04 17:07:25 +0200

    Update to ppxlib >=0.11.0
    Tested with 0.11.0 and 0.12.0
    
    Fixes #73

--- jsx/brisk_ppx.ml
+++ jsx/brisk_ppx.ml
@@ -35,7 +35,7 @@ module JSX_ppx = struct
     let args = props_filter_children props in
     ATH.Exp.apply ~loc ~attrs (component_ident ~loc) args
 
-  let is_jsx ({ AT.txt }, _) = String.equal txt "JSX"
+  let is_jsx { P.attr_name = { txt } } = String.equal txt "JSX"
 
   let filter_jsx = List.filter is_jsx
 
